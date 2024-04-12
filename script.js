f7c0cbbab7734c4b1fd85d6360ef322401d72044
Maximiliano Tis <maxi_tis@hotmail.com>
2024-04-12 01:50:22 -0300

    agregamos para que se vea la Humedad

--- script.js
+++ script.js
@@ -28,6 +28,7 @@ function mostrarDatosClima(data){
     const ciudadNombre = data.name
     const paisNombre = data.sys.country
     const temperatura = data.main.temp
+    const humedad = data.main.humidity
     const descripcion = data.weather[0].description
 
     const ciudadTitulo = document.createElement('h2')
@@ -36,11 +37,15 @@ function mostrarDatosClima(data){
     const temperaturaInfo = document.createElement('p')
     temperaturaInfo.textContent = `La temperatura es: ${Math.floor(temperatura-difKelvin)} °C`
 
+    const humedadInfo = document.createElement('p')
+    humedadInfo.textContent = `La humedad es: ${humedad}%`
+
     const descripcionInfo = document.createElement('p')
     descripcionInfo.textContent = `La descripcion es: ${descripcion}`
 
     divDatosClima.appendChild(ciudadTitulo)
     divDatosClima.appendChild(temperaturaInfo)
+    divDatosClima.appendChild(humedadInfo)
     divDatosClima.appendChild(descripcionInfo)
 
 
