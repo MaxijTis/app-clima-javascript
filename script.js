c5228467d1976ee105514262fd9816c4236eee8f
Maximiliano Tis <maxi_tis@hotmail.com>
2024-04-12 01:42:11 -0300

    Arreglos de errores con la api y finalizacion del proyecto

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
 let api_key = '3f10485cf6803331ed706484022d2931';
+//https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+//api.openweathermap.org/data/3.0/onecall?lat=38.8&lon=12.09&callback=test
+let difKelvin = 273.15
+let urlBase= 'https://api.openweathermap.org/data/2.5/weather'
+
+let ciudad = 'Paris'
+
+
+document.getElementById('botonBusqueda').addEventListener('click', () => {
+    const ciudad = document.getElementById('ciudadEntrada').value
+    if (ciudad) {
+        fetchDatosClima(ciudad)        
+    }
+})
+
+function fetchDatosClima(ciudad){
+    fetch(`${urlBase}?q=${ciudad}&appid=${api_key}`)
+    .then(data => data.json())
+    .then(data => mostrarDatosClima(data))
+}
+
+
+function mostrarDatosClima(data){
+    const divDatosClima = document.getElementById('datosClima')
+    divDatosClima.innerHTML=''
+
+    const ciudadNombre = data.name
+    const temperatura = data.main.temp
+    const descripcion = data.weather[0].description
+
+    const ciudadTitulo = document.createElement('h2')
+    ciudadTitulo.textContent = ciudadNombre
+
+    const temperaturaInfo = document.createElement('p')
+    temperaturaInfo.textContent = `La temperatura es: ${Math.floor(temperatura-difKelvin)} °C`
+
+    const descripcionInfo = document.createElement('p')
+    descripcionInfo.textContent = `La descripcion es: ${descripcion}`
+
+    divDatosClima.appendChild(ciudadTitulo)
+    divDatosClima.appendChild(temperaturaInfo)
+    divDatosClima.appendChild(descripcionInfo)
+
+
+
+
+}
